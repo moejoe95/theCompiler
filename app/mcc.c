ca9ac0d76f7e4cc7d6a3d49da8fd7f410736f8d6
Tobias Kupek <mail@tkupek.eu>
2019-05-06 22:16:00 +0200

    implement mcc app

--- app/mcc.c
+++ app/mcc.c
@@ -1,55 +1,130 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 #include "mcc/ast.h"
+#include "mcc/ast_print.h"
 #include "mcc/parser.h"
+#include "mcc/symbol_table.h"
+#include "mcc/type_checking.h"
+#include <getopt.h>
+#include <string.h>
 
-void print_usage(const char *prg)
+void print_help(const char *prg_name)
 {
-	printf("usage: %s <FILE>\n\n", prg);
-	printf("  <FILE>        Input filepath or - for stdin\n");
+	printf("usage: %s [OPTIONS] file...\n", prg_name);
+	printf("\nThe mc compiler. \n");
+	printf("\nUse '-' as input file to read from stdin.\n");
+	printf("OPTIONS:\n");
+	printf("\t-h,--help \t\tdisplays this help message\n");
+	printf("\t-o,--output <file> \twrite the output to <file> (defaults to stdout)\n");
+	printf("\t-f, --function <name> \tlimit scope to the given function\n");
 }
-
 int main(int argc, char *argv[])
 {
-	if (argc < 2) {
-		print_usage(argv[0]);
-		return EXIT_FAILURE;
-	}
+	static struct option long_options[] = {{"help", no_argument, NULL, 'h'},
+	                                       {"output", optional_argument, NULL, 'o'},
+	                                       {"function", optional_argument, NULL, 'f'}};
 
-	// determine input source
-	FILE *in;
-	if (strcmp("-", argv[1]) == 0) {
-		in = stdin;
+	char func_name_scope[64] = {0};
+	int isScoped = 0;
+	char outfile[64] = {0};
+	snprintf(outfile, sizeof(outfile), "%s", "-");
+	int c;
+	while ((c = getopt_long(argc, argv, "ho:f:", long_options, NULL)) != -1)
+		switch (c) {
+		case 'h':
+			print_help(argv[0]);
+			return EXIT_SUCCESS;
+			break;
+		case 'o':
+			snprintf(outfile, sizeof(outfile), "%s", optarg);
+			break;
+		case 'f':
+			isScoped = 1;
+			snprintf(func_name_scope, sizeof(func_name_scope), "%s", optarg);
+			break;
+		default:
+			return EXIT_FAILURE;
+		}
+
+	// get output file / stdout
+	FILE *out;
+	if (strcmp("-", outfile) == 0) {
+		out = stdout;
 	} else {
-		in = fopen(argv[1], "r");
-		if (!in) {
+		out = fopen(outfile, "w");
+		if (!out) {
 			perror("fopen");
 			return EXIT_FAILURE;
 		}
 	}
 
-	struct mcc_ast_program *pro = NULL;
+	for (int i = optind; i < argc; i++) { // loop through given files
 
-	// parsing phase
-	{
-		struct mcc_parser_result result = mcc_parse_file(in, argv[1], stdout);
-		fclose(in);
-		if (result.status != MCC_PARSER_STATUS_OK) {
+		// get input file / stdin
+		FILE *in;
+		if (strcmp("-", argv[i]) == 0) {
+			in = stdin;
+		} else {
+			in = fopen(argv[i], "r");
+			if (!in) {
+				perror("fopen");
+				return EXIT_FAILURE;
+			}
+		}
+
+		struct mcc_ast_program *pro = NULL;
+
+		// parsing phase
+		{
+			struct mcc_parser_result result = mcc_parse_file(in, argv[i], out);
+
+			if (result.status != MCC_PARSER_STATUS_OK) {
+				fprintf(stdout, "...parsing failed...\n");
+				return EXIT_FAILURE;
+			}
+			pro = result.program;
+		}
+
+		if (isScoped) {
+			struct mcc_ast_func_list *list = pro->function_list;
+			struct mcc_ast_func_list *scope_func_list;
+			while (list != NULL) {
+				char *id = list->function->func_identifier->identifier->name;
+				if (strcmp(func_name_scope, id) == 0) {
+					scope_func_list = list;
+				}
+				list = list->next_function;
+			}
+			pro->function_list = scope_func_list;
+			pro->function_list->next_function = NULL;
+		}
+		mcc_ast_print_dot(out, pro);
+
+		// build symbol table
+		struct mcc_symbol_table *st = NULL;
+		st = mcc_create_symbol_table(pro, out);
+		if (st == NULL) {
 			return EXIT_FAILURE;
 		}
-		pro = result.program;
+		mcc_print_symbol_table(out, st);
+		fprintf(out, "\n");
+
+		// type checking
+		mcc_check_types(pro, st, out, 1);
+
+		// cleanup
+		mcc_delete_symbol_table(st);
+		mcc_ast_delete_program(pro);
+
+		if (fclose(in) != 0) {
+			perror("fclose input");
+			return EXIT_FAILURE;
+		}
 	}
-
-	// TODO:
-	// - run semantic checks
-	// - create three-address code
-	// - output assembly code
-	// - invoke backend compiler
-
-	// cleanup
-	mcc_ast_delete(pro);
-
+	if (fclose(out) != 0) {
+		perror("fclose output");
+		return EXIT_FAILURE;
+	}
 	return EXIT_SUCCESS;
 }
--- src/print_type_log.c
+++ src/print_type_log.c
@@ -1,85 +1,47 @@
 #include "mcc/print_type_log.h"
 #include "mcc/error_handler.h"
 
-
-
 void mcc_print_type_log_header(FILE *out)
 {
-    assert(out);
-
-	fprintf(out, "trace type checking ...\n");
-	fprintf(out, "\nLocation\t|\tExpression\t|\tType-LHS - Type-RHS\t|\tStatus\n");
-    fprintf(out, "--------------------------------------------------------------------------------------\n");
+	assert(out);
+	fprintf(out, "Location\t|\tExpression\t|\tType-LHS - Type-RHS\t|\tStatus\n");
+	fprintf(out, "--------------------------------------------------------------------------------------\n");
 }
 
-void mcc_print_type_log_assign(FILE *out, struct mcc_type_log *log, char *expr){
-    fprintf(out, "%d:%d\t\t|\t%s\t|\t%s <- %s\t\t|\t%s\n", 
-        log->sloc->end_line+1, 
-        log->sloc->end_col+1,
-        expr, 
-        get_type_string(log->lhs_type), 
-        get_type_string(log->rhs_type), 
-        get_status_string(log->status)
-        );
-}
-
-void mcc_print_type_log_return(FILE *out, struct mcc_type_log *log, char *expr){
-    fprintf(out, "%d:%d\t\t|\t%s\t\t|\t%s ~ %s\t\t|\t%s\n", 
-        log->sloc->end_line+1, 
-        log->sloc->end_col+1,
-        expr, 
-        get_type_string(log->lhs_type), 
-        get_type_string(log->rhs_type), 
-        get_status_string(log->status)
-        );
-}
-
-void mcc_print_type_log_bin(FILE *out, struct mcc_type_log *log, char *expr){
-    fprintf(out, "%d:%d\t\t|\t%s\t\t|\t%s ~ %s\t\t|\t%s\n", 
-        log->sloc->end_line+1, 
-        log->sloc->end_col+1,
-        expr, 
-        get_type_string(log->lhs_type), 
-        get_type_string(log->rhs_type), 
-        get_status_string(log->status)
-        );
-}
-
-void mcc_print_type_log_op(FILE *out, struct mcc_type_log *log, char *expr){
-    fprintf(out, "%d:%d\t\t|\t%s\t\t|\t%s op %s\t\t|\t%s\n", 
-        log->sloc->end_line+1, 
-        log->sloc->end_col+1,
-        expr, 
-        get_type_string(log->lhs_type), 
-        get_type_string(log->rhs_type), 
-        get_status_string(log->status)
-        );
-}
-
-void mcc_print_type_log_u_op(FILE *out, struct mcc_type_log *log, char *expr){
-    fprintf(out, "%d:%d\t\t|\t%s\t\t|\top %s\t\t|\t%s\n", 
-        log->sloc->end_line+1, 
-        log->sloc->end_col+1,
-        expr, 
-        get_type_string(log->lhs_type), 
-        get_status_string(log->status)
-        );
-}
-
-void mcc_print_type_log_decl(FILE *out, struct mcc_type_log *log, char *expr){
-    fprintf(out, "%d:%d\t\t|\t%s\t|\t%s\t\t\t|\t%s\n", 
-        log->sloc->end_line+1, 
-        log->sloc->end_col+1,
-        expr, 
-        get_type_string(log->lhs_type), 
-        get_status_string(log->status)
-        );
-}
-
-void mcc_print_type_log_footer(FILE *out)
+void mcc_print_type_log_assign(FILE *out, struct mcc_type_log *log, char *expr)
 {
-    assert(out);
-	fprintf(out, "\n");
+	fprintf(out, "%d:%d\t\t|\t%s\t|\t%s <- %s\t\t|\t%s\n", log->sloc->end_line + 1, log->sloc->end_col + 1, expr,
+	        get_type_string(log->lhs_type), get_type_string(log->rhs_type), get_status_string(log->status));
+}
+
+void mcc_print_type_log_return(FILE *out, struct mcc_type_log *log, char *expr)
+{
+	fprintf(out, "%d:%d\t\t|\t%s\t\t|\t%s ~ %s\t\t|\t%s\n", log->sloc->end_line + 1, log->sloc->end_col + 1, expr,
+	        get_type_string(log->lhs_type), get_type_string(log->rhs_type), get_status_string(log->status));
+}
+
+void mcc_print_type_log_bin(FILE *out, struct mcc_type_log *log, char *expr)
+{
+	fprintf(out, "%d:%d\t\t|\t%s\t\t|\t%s ~ %s\t\t|\t%s\n", log->sloc->end_line + 1, log->sloc->end_col + 1, expr,
+	        get_type_string(log->lhs_type), get_type_string(log->rhs_type), get_status_string(log->status));
+}
+
+void mcc_print_type_log_op(FILE *out, struct mcc_type_log *log, char *expr)
+{
+	fprintf(out, "%d:%d\t\t|\t%s\t\t|\t%s op %s\t\t|\t%s\n", log->sloc->end_line + 1, log->sloc->end_col + 1, expr,
+	        get_type_string(log->lhs_type), get_type_string(log->rhs_type), get_status_string(log->status));
+}
+
+void mcc_print_type_log_u_op(FILE *out, struct mcc_type_log *log, char *expr)
+{
+	fprintf(out, "%d:%d\t\t|\t%s\t\t|\top %s\t\t|\t%s\n", log->sloc->end_line + 1, log->sloc->end_col + 1, expr,
+	        get_type_string(log->lhs_type), get_status_string(log->status));
+}
+
+void mcc_print_type_log_decl(FILE *out, struct mcc_type_log *log, char *expr)
+{
+	fprintf(out, "%d:%d\t\t|\t%s\t|\t%s\t\t\t|\t%s\n", log->sloc->end_line + 1, log->sloc->end_col + 1, expr,
+	        get_type_string(log->lhs_type), get_status_string(log->status));
 }
 
 struct mcc_type_log *get_mcc_type_log_struct(enum mcc_type_status status)
