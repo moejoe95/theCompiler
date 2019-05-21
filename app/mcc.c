ec0f2bf06b8f755f28475da4b7d0c57cdfe3643e
csat2151 <Josef.Gugglberger@student.uikb.ac.at>
2019-05-21 14:49:28 +0200

    add IR to mcc

--- app/mcc.c
+++ app/mcc.c
@@ -144,6 +144,9 @@ int main(int argc, char *argv[])
 		// type checking
 		mcc_check_types(pro, st, out, log_level_to_int(LOG_LEVEL));
 
+		// generate IR code
+		mcc_create_ir(pro, out, log_level_to_int(LOG_LEVEL));
+
 		// cleanup
 		mcc_delete_symbol_table(st);
 		mcc_ast_delete_program(pro);
