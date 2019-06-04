ff9e31fb0208fd19288e688bda8a03688e5d63bd
csat2151 <Josef.Gugglberger@student.uikb.ac.at>
2019-06-04 16:49:57 +0200

    update main asm

--- src/asm.c
+++ src/asm.c
@@ -7,21 +7,23 @@
 
 #include "mcc/ir.h"
 
+void print_asm_instruction(FILE *out, char *op, char *arg1, char *arg2)
+{
+	assert(op);
+	if (arg1 == NULL && arg2 == NULL)
+		fprintf(out, "\t%s\n", op);
+	else if (arg2 == NULL)
+		fprintf(out, "\t%s\t%s\n", op, arg1);
+	else
+		fprintf(out, "\t%s\t%s, %s\n", op, arg1, arg2);
+}
+
 void create_function_label(FILE *out, char *func_name)
 {
 	assert(out);
 	fprintf(out, "%s:\n", func_name);
-}
-
-void print_asm_instruction(FILE *out, char *op, char *arg1, char *arg2)
-{
-	assert(op);
-	if (arg2 == NULL)
-		fprintf(out, "\t%s\t%s\n", op, arg1);
-	else if (arg1 == NULL)
-		fprintf(out, "\t%s\n", op);
-	else
-		fprintf(out, "\t%s\t%s, %s\n", op, arg1, arg2);
+	print_asm_instruction(out, "pushl", "%ebp", NULL);
+	print_asm_instruction(out, "movl", "%esp", "%ebp");
 }
 
 void create_asm_return(FILE *out, struct mcc_ir_line *line)
@@ -57,8 +59,15 @@ void mcc_create_asm(struct mcc_ir_table_head *ir, FILE *out)
 			create_asm_line(out, current_line);
 			current_line = current_line->next_line;
 		}
+		if (strcmp(current_func->func_name, "main") == 0) {
+			print_asm_instruction(out, "leave", NULL, NULL);
+			print_asm_instruction(out, "retl", NULL, NULL);
+		}
 		current_func = current_func->next_table;
 	}
 
-	/* compile assembly with gcc -c file.S -o file.o */
+	/* compile assembly with
+	        gcc -c file.s -o file.o
+	        gcc -o file file.o
+	*/
 }
