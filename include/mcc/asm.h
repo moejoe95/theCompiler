ba36444ecffee5ae68f7d3a7862c64bab26ced9d
csat2151 <Josef.Gugglberger@student.uikb.ac.at>
2019-06-05 11:10:07 +0200

    add dynamic stack allocation

--- include/mcc/asm.h
+++ include/mcc/asm.h
@@ -21,6 +21,10 @@ enum mcc_asm_operand {
     MCC_ASM_REGISTER_ESP,
 };
 
+struct mcc_asm_head {
+    int offset;
+};
+
 void mcc_create_asm(struct mcc_ir_table_head *ir, FILE *out);
 
 #endif
--- src/asm.c
+++ src/asm.c
@@ -14,14 +14,22 @@ EBP is used as the base register to reference the local variables, and a value i
 on the stack. In this case, eight bytes have been reserved on the stack. Note that pushl automatically decremented ESP
 by the appropriate length.
 */
-void create_function_label(FILE *out, char *func_name)
+void create_function_label(FILE *out, struct mcc_ir_table *current_func)
 {
 	assert(out);
-	fprintf(out, "\t.globl %s\n\n", func_name);
-	fprintf(out, "%s:\n", func_name);
+	fprintf(out, "\t.globl %s\n\n", current_func->func_name);
+	fprintf(out, "%s:\n", current_func->func_name);
+	int memory_size = 0;
+	struct mcc_ir_line *current_line = current_func->line_head->root;
+	while (current_line != NULL) {
+		memory_size = memory_size + 4 * current_line->memory_size;
+		current_line = current_line->next_line;
+	}
+	char memory_size_str[12];
+	sprintf(memory_size_str, "%d", memory_size);
 	print_asm_instruction_reg(out, MCC_ASM_INSTRUCTION_PUSHL, MCC_ASM_REGISTER_EBP, 0, -1, 0);
 	print_asm_instruction_reg(out, MCC_ASM_INSTRUCTION_MOVL, MCC_ASM_REGISTER_ESP, 0, MCC_ASM_REGISTER_EBP, 0);
-	print_asm_instruction_lit(out, MCC_ASM_INSTRUCTION_SUBL, "8", MCC_ASM_REGISTER_ESP, 0);
+	print_asm_instruction_lit(out, MCC_ASM_INSTRUCTION_SUBL, memory_size_str, MCC_ASM_REGISTER_ESP, 0);
 }
 
 /*
@@ -35,29 +43,34 @@ void create_asm_return(FILE *out, struct mcc_ir_line *line)
 	print_asm_instruction_lit(out, MCC_ASM_INSTRUCTION_MOVL, line->arg1, MCC_ASM_REGISTER_EAX, 0);
 }
 
-void create_asm_binary_op(FILE *out, struct mcc_ir_line *line)
+void create_asm_binary_op(FILE *out, struct mcc_ir_line *line, struct mcc_asm_head *asm_head)
 {
+	asm_head->offset = asm_head->offset - 4;
 	switch (line->bin_op) {
 	case MCC_AST_BINARY_OP_ADD:
 		print_asm_instruction_lit(out, MCC_ASM_INSTRUCTION_MOVL, line->arg1, MCC_ASM_REGISTER_EAX, 0);
 		print_asm_instruction_lit(out, MCC_ASM_INSTRUCTION_ADDL, line->arg2, MCC_ASM_REGISTER_EAX, 0);
 		print_asm_instruction_reg(out, MCC_ASM_INSTRUCTION_MOVL, MCC_ASM_REGISTER_EAX, 0, MCC_ASM_REGISTER_EBP,
-		                          -4);
+		                          asm_head->offset);
 		break;
-
+	case MCC_AST_BINARY_OP_SUB:
+		print_asm_instruction_lit(out, MCC_ASM_INSTRUCTION_MOVL, line->arg1, MCC_ASM_REGISTER_EAX, 0);
+		print_asm_instruction_lit(out, MCC_ASM_INSTRUCTION_SUBL, line->arg2, MCC_ASM_REGISTER_EAX, 0);
+		print_asm_instruction_reg(out, MCC_ASM_INSTRUCTION_MOVL, MCC_ASM_REGISTER_EAX, 0, MCC_ASM_REGISTER_EBP,
+		                          asm_head->offset);
 	default:
 		break;
 	}
 }
 
-void create_asm_line(FILE *out, struct mcc_ir_line *line)
+void create_asm_line(FILE *out, struct mcc_ir_line *line, struct mcc_asm_head *asm_head)
 {
 	assert(out);
 	assert(line);
 
 	switch (line->op_type) {
 	case MCC_IR_TABLE_BINARY_OP:
-		create_asm_binary_op(out, line);
+		create_asm_binary_op(out, line, asm_head);
 		break;
 	case MCC_IR_TABLE_RETURN:
 		create_asm_return(out, line);
@@ -73,12 +86,15 @@ void mcc_create_asm(struct mcc_ir_table_head *ir, FILE *out)
 
 	create_asm_header(out);
 
+	struct mcc_asm_head *asm_head = malloc(sizeof(*asm_head));
+	asm_head->offset = 0;
+
 	struct mcc_ir_table *current_func = ir->root;
 	while (current_func != NULL) {
 		struct mcc_ir_line *current_line = current_func->line_head->root;
-		create_function_label(out, current_func->func_name);
+		create_function_label(out, current_func);
 		while (current_line != NULL) {
-			create_asm_line(out, current_line);
+			create_asm_line(out, current_line, asm_head);
 			current_line = current_line->next_line;
 		}
 		if (strcmp(current_func->func_name, "main") == 0) {
