2112e1c4e6955e332ba5b380579dc493630a2812
csat2151 <Josef.Gugglberger@student.uikb.ac.at>
2019-07-10 14:27:40 +0200

    replace line number references

--- src/asm.c
+++ src/asm.c
@@ -722,7 +722,9 @@ void create_asm_array_assignment(FILE *out, struct mcc_ir_line *line, struct mcc
 		print_asm_instruction_load_float(out, MCC_ASM_INSTRUCTION_FLDS, index);
 		print_asm_instruction_store_float(out, MCC_ASM_INSTRUCTION_FSTPS, MCC_ASM_REGISTER_EBP, stack_position);
 	} else if (line->memory_size == 1) { // int arrays
-		print_asm_instruction_lit(out, MCC_ASM_INSTRUCTION_MOVL, line->arg2, MCC_ASM_REGISTER_EAX, 0);
+		int stack_position_arg2 = find_stack_position(line->arg2, head->stack);
+		print_asm_instruction_reg(out, MCC_ASM_INSTRUCTION_MOVL, MCC_ASM_REGISTER_EBP, stack_position_arg2,
+		                          MCC_ASM_REGISTER_EAX, 0);
 		print_asm_instruction_array_set(out, MCC_ASM_INSTRUCTION_MOVL, MCC_ASM_REGISTER_EAX, index);
 		print_asm_instruction_array_get(out, MCC_ASM_INSTRUCTION_MOVL, index, MCC_ASM_REGISTER_EAX);
 		print_asm_instruction_reg(out, MCC_ASM_INSTRUCTION_MOVL, MCC_ASM_REGISTER_EAX, 0, MCC_ASM_REGISTER_EBP,
