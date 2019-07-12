48c220f43c2b3fecdf09fdfe2ec670529c41768a
Tobias Kupek <tobias.kupek@speed-u-up.at>
2019-07-12 11:32:57 +0200

    fix stack memory leak

--- src/asm.c
+++ src/asm.c
@@ -1074,14 +1074,15 @@ void mcc_create_asm(struct mcc_ir_table_head *ir, FILE *out, int destination)
 		current_func = current_func->next_table;
 	}
 
+	mcc_delete_stack(asm_head->stack);
+	asm_head->stack = NULL;
+
 	current_func = ir->root;
 	while (current_func != NULL) {
 		if (strcmp(current_func->func_name, "main") != 0) {
 			struct mcc_ir_line *current_line = current_func->line_head->root;
 			asm_head->offset = 0;
 			asm_head->ir = current_func;
-			mcc_delete_stack(asm_head->stack);
-			asm_head->stack = NULL;
 			create_function_label(tmpfile, current_func, asm_head);
 			while (current_line != NULL) {
 				create_asm_line(tmpfile, current_line, asm_head, current_func);
@@ -1089,6 +1090,7 @@ void mcc_create_asm(struct mcc_ir_table_head *ir, FILE *out, int destination)
 			}
 		}
 		current_func = current_func->next_table;
+		mcc_delete_stack(asm_head->stack);
 		asm_head->stack = NULL;
 	}
 
