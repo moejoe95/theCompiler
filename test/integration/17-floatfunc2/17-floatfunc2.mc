6dce4756681d09adf8ebc960f64304e5245897c9
csat2151 <Josef.Gugglberger@student.uikb.ac.at>
2019-07-19 14:11:38 +0200

    fix floatfunc2 test

--- test/integration/17-floatfunc2/17-floatfunc2.mc
+++ test/integration/17-floatfunc2/17-floatfunc2.mc
@@ -8,11 +8,12 @@ int main()
 	float fx;
 	x = 0.0;
 
-	while (x < 80.0) {
+	while (x < 5.0) {
 		fx = transform(x);
 		x = x + 1.0;
 
 		print_float(x);
+		print_nl();
 	}
 
 	return 0;
--- test/integration/17-floatfunc2/17-floatfunc2.s
+++ test/integration/17-floatfunc2/17-floatfunc2.s
@@ -5,97 +5,69 @@
 main:
         pushl   %ebp
         movl    %esp, %ebp
-        subl    $140, %esp
-        movl    $0, %edi
-        flds    tmp_0
-        fstps   arr(%edi)
-        movl    $4, %edi
+        subl    $68, %esp
+        flds    x_0
+        fstps   -4(%ebp)
+L_main_3:
+        flds    -4(%ebp)
         flds    tmp_1
-        fstps   arr(%edi)
-        movl    $8, %edi
+        fcomip  %st(1), %st
+        fstp    %st(0)
+        seta    %al
+        movzbl  %al, %eax
+        movl    %eax, -8(%ebp)
+        cmpl    $0, %eax
+        je      L_main_14
+        flds    -4(%ebp)
+        fstps   -12(%ebp)
+        pushl   -12(%ebp)
+        calll   transform
+        addl    $4, %esp
+        movl    %eax, -20(%ebp)
+        movl    -20(%ebp), %eax
+        movl    %eax, -24(%ebp)
         flds    tmp_2
-        fstps   arr(%edi)
-        movl    $0, %edi
-        pushl   arr(%edi)
-        calll   print_float
-        calll   print_nl
-        movl    $4, %edi
-        pushl   arr(%edi)
-        calll   print_float
-        calll   print_nl
-        movl    $8, %edi
-        pushl   arr(%edi)
-        calll   print_float
-        calll   print_nl
-        movl    $4, %edi
-        flds    tmp_3
-        fstps   arr(%edi)
-        movl    $4, %edi
-        pushl   arr(%edi)
-        calll   print_float
-        calll   print_nl
-        movl    $8, %edi
-        flds    tmp_4
-        fstps   arr(%edi)
-        movl    $4, %edi
-        movl    $8, %edi
-        movl    $4, %edi
-        flds    arr(%edi)
-        movl    $8, %edi
-        flds    arr(%edi)
-        fmulp
-        fstps   -24(%ebp)
-        flds    -24(%ebp)
-        fstps   -28(%ebp)
-        pushl   -28(%ebp)
-        calll   print_float
-        addl    $4, %esp
-        calll   print_nl
-        movl    $4, %edi
-        flds    arr(%edi)
-        fstps   -32(%ebp)
-        flds    -32(%ebp)
-        fstps   -36(%ebp)
-        pushl   -36(%ebp)
-        calll   print_float
-        addl    $4, %esp
-        calll   print_nl
-        movl    $0, %edi
-        movl    $8, %edi
-        movl    $0, %edi
-        flds    arr(%edi)
-        movl    $8, %edi
-        flds    arr(%edi)
+        flds    -4(%ebp)
         faddp
-        fstps   -40(%ebp)
-        movl    -40(%ebp), %eax
-        movl    %eax, -44(%ebp)
-        flds    -44(%ebp)
-        fstps   -48(%ebp)
-        pushl   -48(%ebp)
+        fstps   -28(%ebp)
+        movl    -28(%ebp), %eax
+        movl    %eax, -4(%ebp)
+        flds    -28(%ebp)
+        fstps   -32(%ebp)
+        pushl   -32(%ebp)
         calll   print_float
         addl    $4, %esp
         calll   print_nl
+        jmp     L_main_3
+L_main_14:
         movl    $0, %eax
         leave
         ret
 
+        .globl transform
+
+transform:
+        pushl   %ebp
+        movl    %esp, %ebp
+        subl    $16, %esp
+        movl    8(%ebp), %eax
+        movl    %eax, -4(%ebp)
+        movl    tmp_3, %eax
+        addl    $16, %esp
+        popl    %ebp
+        ret
+
 .data
 
-arr:
-        .float 0.0, 0.0, 0.0
-
-tmp_0:
+x_0:
         .float 0.000000
 
 tmp_1:
-        .float 1.000000
+        .float 5.000000
 
 tmp_2:
-        .float 2.000000
+        .float 1.000000
 
 tmp_3:
-        .float 9.000000
-
-tmp_4:
-        .float 2.000000
+        .float 1.000000
+        
